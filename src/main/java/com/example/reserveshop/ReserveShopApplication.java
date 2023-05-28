e11a036a1a7637ec3adb21baacde22ce27be1341
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-29 00:55:23 +0900

    chore: init

---
+++ src/main/java/com/example/reserveshop/ReserveShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.reserveshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reserveshop/ReserveShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reserveshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
