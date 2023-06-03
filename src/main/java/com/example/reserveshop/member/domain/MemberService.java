f4844cc7710ba9cadf1da22ca2f5573110c5399e
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-03 15:04:04 +0900

    chore: db id 수정

--- src/main/java/com/example/reserveshop/member/domain/MemberService.java
+++ src/main/java/com/example/reserveshop/member/domain/MemberService.java
@@ -12,6 +12,11 @@ public class MemberService {
     private static final String USER_NOT_FOUND = "회원이 존재하지 않습니다.";
     private final MemberRepository memberRepository;
 
+    /**
+     * 회원가입
+     * @param request
+     * @return
+     */
     public Long joinMember(CreateMemberRequest request) {
         Member member = memberRepository.save(Member.builder()
                 .loginId(request.getLoginId())
@@ -23,6 +28,11 @@ public class MemberService {
         return member.getId();
     }
 
+    /**
+     * 회원을 조회합니다
+     * @param id
+     * @return
+     */
     public MemberInfo getMember(Long id) {
         return MemberInfo.fromEntity(memberRepository.findById(id)
                         .orElseThrow(() -> new IllegalArgumentException(USER_NOT_FOUND)));
