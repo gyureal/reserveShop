e9b5c88772b59d35b6d53ec2b134af94a8d0452e
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-04 19:11:54 +0900

    feat(reservation): 예약 조회

--- src/test/java/com/example/reserveshop/integrationTest/ReservationIntegrationTest.java
+++ src/test/java/com/example/reserveshop/integrationTest/ReservationIntegrationTest.java
@@ -4,6 +4,9 @@ import com.example.reserveshop.member.domain.Member;
 import com.example.reserveshop.member.domain.MemberRepository;
 import com.example.reserveshop.member.vo.*;
 import com.example.reserveshop.member.web.dto.CreateMemberRequest;
+import com.example.reserveshop.reservation.domain.entity.Reservation;
+import com.example.reserveshop.reservation.domain.repository.ReservationRepository;
+import com.example.reserveshop.reservation.domain.vo.ReserveStatus;
 import com.example.reserveshop.reservation.web.dto.CreateReservationRequest;
 import com.example.reserveshop.reservation.web.dto.ReservationInfo;
 import com.example.reserveshop.store.domain.entity.Store;
@@ -16,16 +19,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 
+import java.time.*;
+import java.util.List;
+
+import static com.example.reserveshop.reservation.domain.vo.ReserveStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class ReservationIntegrationTest extends IntegrationTest {
+
+    private static final Clock FIXED_CLOCK = Clock.fixed(Instant.parse("2020-03-10T12:00:00.000Z"), ZoneOffset.UTC);
+    private static final String PHONE_NUMBER = "010-0000-0000";
 
     @Autowired
     MemberRepository memberRepository;
     @Autowired
     StoreRepository storeRepository;
+    @Autowired
+    ReservationRepository reservationRepository;
+    @SpyBean
+    private Clock clock;
 
     private Member member1;
     private Store store1;
@@ -77,10 +93,13 @@ public class ReservationIntegrationTest extends IntegrationTest {
     void createReservationSuccess() {
         // given
         CreateReservationRequest request = CreateReservationRequest.builder()
-                .storeId(2L)
-                .memberId(1L)
-                .phoneNumber("010-1111-1111")
+                .storeId(store1.getId())
+                .memberId(member1.getId())
+                .phoneNumber(PHONE_NUMBER)
                 .build();
+
+        doReturn(Instant.now(FIXED_CLOCK))
+                .when(clock).instant();
 
         // when
         ExtractableResponse<Response> response = RestAssured
@@ -93,6 +112,11 @@ public class ReservationIntegrationTest extends IntegrationTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(201);
+
+        ReservationInfo result = response.body().as(ReservationInfo.class);
+        LocalDateTime test = LocalDateTime.now(FIXED_CLOCK);
+        ReservationInfo answer = ReservationInfo.fromEntity(makeReservation(member1, store1, REQUEST, LocalDateTime.now(FIXED_CLOCK)));
+        assertThat(result).usingRecursiveComparison().ignoringFields("id", "reserveDateTime").isEqualTo(answer);
     }
 
     @Test
@@ -122,5 +146,52 @@ public class ReservationIntegrationTest extends IntegrationTest {
         assertThat(response.statusCode()).isEqualTo(200);
         assertThat(response.body().as(ReservationInfo.class)).usingRecursiveComparison()
                 .isEqualTo(createdInfo);
+    }
+
+    @Test
+    @DisplayName("특정 상점의 예약을 날짜 범위로 조회합니다.")
+    void searchReservationsByStoreIdAndDateRange() {
+        // given
+        ReservationInfo reservation1 = insertReservation(member1, store1, REQUEST, LocalDateTime.parse("2020-03-10T12:00:00"));
+        ReservationInfo reservation2 = insertReservation(member1, store1, REQUEST, LocalDateTime.parse("2020-03-12T13:00:00"));
+        ReservationInfo reservation3 = insertReservation(member1, store1, REQUEST, LocalDateTime.parse("2020-03-14T14:00:00"));
+
+        Long storeId = store1.getId();
+        ReserveStatus status = REQUEST;
+        String from = "2020-03-10";
+        String to = "2020-03-12";
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .queryParam("storeId", storeId)
+                .queryParam("status", status)
+                .queryParam("from", from)
+                .queryParam("to", to)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/reservations")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(200);
+
+        List<ReservationInfo> result = response.body().jsonPath().getList(".", ReservationInfo.class);
+        List<ReservationInfo> answer = List.of(reservation1, reservation2);
+        assertThat(result).usingRecursiveComparison().isEqualTo(answer);
+    }
+
+    private ReservationInfo insertReservation(Member member, Store store, ReserveStatus status, LocalDateTime dateTime) {
+        return ReservationInfo.fromEntity(reservationRepository.save(makeReservation(member, store, status, dateTime)));
+    }
+
+    private Reservation makeReservation(Member member, Store store, ReserveStatus status, LocalDateTime dateTime) {
+        return Reservation.builder()
+                .member(member)
+                .store(store)
+                .phoneNumber(PhoneNumber.of(PHONE_NUMBER))
+                .status(status)
+                .reserveDateTime(dateTime)
+                .build();
     }
 }
